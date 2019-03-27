99b0921719712ed30bc53eb2960c9fe4b116ff05
Jorge García-Quesada Barahona <jgqb26@gmail.com>
2019-03-27 10:43:58 +0100

    Plantilla lista sin chorradas

---
+++ PB/src/pb/PB.java
@@ -0,0 +1,60 @@
+package pb;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Null_2Mares
+ */
+public class PB {
+    
+    
+    //ATRIBUTOS GLOBALES    
+    Scanner sc = new Scanner(System.in);
+    int contador;
+    String entrada;
+    List<String> lista = new ArrayList();
+    boolean comprueba;    
+    
+    public static void main(String[] args) {
+        boolean finalizar;
+        do {
+            String entrada = leerEntrada();
+            if (!(finalizar = finalDeJuego())) {
+                logica();
+                escribirSalida();
+            }
+        } while (!finalizar);
+    }
+
+    private static String leerEntrada() {  
+        return null;
+    }
+
+
+    private static void logica() {
+        
+    
+    
+    
+    
+    
+    
+    }
+
+    
+    
+    
+    private static void escribirSalida() {
+        
+    }
+    
+    private static boolean finalDeJuego() {
+        
+    
+    
+        return false;
+    }
+}
---
+++ PC/src/pc/PC.java
@@ -0,0 +1,60 @@
+package pc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Null_2Mares
+ */
+public class PC {
+    
+    
+    //ATRIBUTOS GLOBALES    
+    Scanner sc = new Scanner(System.in);
+    int contador;
+    String entrada;
+    List<String> lista = new ArrayList();
+    boolean comprueba;    
+    
+    public static void main(String[] args) {
+        boolean finalizar;
+        do {
+            String entrada = leerEntrada();
+            if (!(finalizar = finalDeJuego())) {
+                logica();
+                escribirSalida();
+            }
+        } while (!finalizar);
+    }
+
+    private static String leerEntrada() {  
+        return null;
+    }
+
+
+    private static void logica() {
+        
+    
+    
+    
+    
+    
+    
+    }
+
+    
+    
+    
+    private static void escribirSalida() {
+        
+    }
+    
+    private static boolean finalDeJuego() {
+        
+    
+    
+        return false;
+    }
+}
