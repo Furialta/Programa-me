090152a220bfcb1e1d013f54477e98ab8e296ce5
Hector Ibañez <7946424@alu.murciaeduca.es>
2019-03-27 10:32:53 +0100

    Plantilla001

--- Ejercicio0/src/ejercicio0/Ejercicio0.java
+++ Ejercicio0/src/ejercicio0/Ejercicio0.java
@@ -1,5 +1,7 @@
 package ejercicio0;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -7,24 +9,15 @@ import java.util.Scanner;
  * @author Null_2Mares
  */
 public class Ejercicio0 {
-
-    private static String leerEntrada() {
-        
-    }
-
-    private static boolean finalDeJuego() {
-        
-    }
-
-    private static void logica() {
-        
-    }
-
-    private static void escribirSalida() {
-        
-    }
-        Scanner sc = new Scanner(System.in);
-
+    
+    
+    //ATRIBUTOS GLOBALES    
+    Scanner sc = new Scanner(System.in);
+    int contador;
+    String entrada;
+    List<String> lista = new ArrayList();
+    boolean comprueba;    
+    
     public static void main(String[] args) {
         boolean finalizar;
         do {
@@ -34,5 +27,40 @@ public class Ejercicio0 {
                 escribirSalida();
             }
         } while (!finalizar);
+    }
+
+    private static String leerEntrada() {
+        
+    
+    
+    
+    
+    
+        return null;
+    }
+
+
+    private static void logica() {
+        
+    
+    
+    
+    
+    
+    
+    }
+
+    
+    
+    
+    private static void escribirSalida() {
+        
+    }
+    
+    private static boolean finalDeJuego() {
+        
+    
+    
+        return null;
     }
 }
