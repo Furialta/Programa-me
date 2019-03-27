115b986bf36e6675d56667a22090cf4f1f84841d
Jorge García-Quesada Barahona <jgqb26@gmail.com>
2019-03-27 09:31:29 +0100

    Estructura Ejercicio0

---
+++ Ejercicio0/src/ejercicio0/Ejercicio0.java
@@ -0,0 +1,38 @@
+package ejercicio0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Null_2Mares
+ */
+public class Ejercicio0 {
+
+    private static String leerEntrada() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static boolean finalDeJuego() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void logica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void escribirSalida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean finalizar;
+        do {
+            String entrada = leerEntrada();
+            if (!(finalizar = finalDeJuego())) {
+                logica();
+                escribirSalida();
+            }
+        } while (!finalizar);
+    }
+}
